13e513f2e69333f33d0c93b6684011d7ba91b70e
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-10-18 04:05:43 +0700

    use php artisan serve with spesifik test case

--- belajar-laravel-dasar/tests/Feature/ContohTest.php
+++ belajar-laravel-dasar/tests/Feature/ContohTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+// if only test ContohTest in Feature you can use = php artisan test --testsuite=Feature --filter ContohTest
 class ContohTest extends TestCase
 {
     /**
