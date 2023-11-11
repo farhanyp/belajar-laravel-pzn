72a4d51cd4415f19f8df1b5dff1bad670eb12f79
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-11-11 14:16:17 +0700

    Insert Many Data

--- eloquent-app/tests/Feature/CategoryTest.php
+++ eloquent-app/tests/Feature/CategoryTest.php
@@ -3,12 +3,20 @@
 namespace Tests\Feature;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
+
+    public function setUp(): void{
+
+        parent::setUp();
+        DB::delete('DELETE from categories');
+        
+    }
     
     public function testInsert(): void
     {
@@ -18,5 +26,23 @@ class CategoryTest extends TestCase
         $result = $category->save();
 
         self::assertTrue($result);
+    }
+
+    public function testInsertMany(): void
+    {
+        $categories = [];
+        for ($i=1; $i <= 10; $i++) {
+            $categories[] = [
+                "id" => "category-{$i}",
+                "name" => "category {$i}"
+            ];
+        }
+
+        $result = Category::insert($categories);
+        self::assertTrue($result);
+
+        $count = Category::count();
+        self::assertEquals(10, $count);
+
     }
 }
