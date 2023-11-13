de01923b630b64125a75f4c3ae2afdaccdc1a160
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-11-13 14:51:33 +0700

    Timestamps

---
+++ eloquent-app/tests/Feature/CommentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Comment;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class CommentTest extends TestCase
+{
+
+    public function setUp(): void{
+
+        parent::setUp();
+        DB::delete('DELETE from comments');
+        
+    }
+    
+    public function testCreateComments(): void
+    {
+        $comment = new Comment();
+        $comment->email = "farhan@gmail.com";
+        $comment->title = "Sample Title";
+        $comment->comment = "Sample Comment";
+        $comment->created_at = new \DateTime();
+        $comment->updated_at = new \DateTime();
+        $comment->save();
+
+        self::assertNotNull($comment->id);
+    }
+}
