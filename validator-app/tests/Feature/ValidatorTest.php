1acb6b647bd78aa763667ae4974c49b0b2b0d537
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-11-30 15:12:14 +0700

    Learn Validation

---
+++ validator-app/tests/Feature/ValidatorTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    
+    public function testValidator(): void
+    {
+        $data = [
+            "username" => "admin",
+            "password" => "123456",
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+    }
+}
