06f4445e250733df31b528783f48332ff14d4b1e
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-12-01 19:35:17 +0700

    Learn Validation, Validation Exception

--- validator-app/tests/Feature/ValidatorTest.php
+++ validator-app/tests/Feature/ValidatorTest.php
@@ -4,7 +4,9 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 use Tests\TestCase;
 
 class ValidatorTest extends TestCase
@@ -46,5 +48,34 @@ class ValidatorTest extends TestCase
 
         self::assertFalse($validator->passes());;
         self::assertTrue($validator->fails());
+
+        $message = $validator->getMessageBag();
+        Log::info($message->toJson(JSON_PRETTY_PRINT));
+    }
+
+    public function testValidatorValidationException(): void
+    {
+        $data = [
+            "username" => "",
+            "password" => "12345",
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required",
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        try{
+            // berfungsi untuk cek validasi, jika ada error akan masuk ke exception, jika tidak ada akan lanjut
+            $validator->validate();
+            self::fail("Tidak ada Validation");
+        }catch(ValidationException $exception){
+            self::assertNotNull($exception->validator);
+            $message = $exception->validator->errors();
+            Log::info($message->toJson(JSON_PRETTY_PRINT));
+        }
     }
 }
